2a067f2ecfd9120ead710e41eaf63bf8a4cbb80e
gilett4 <vadymudod@hotmail.com>
2020-10-31 17:44:23 +0200

    swipeHandler editor added

---
+++ Scripts/Editor/SwipeHandlerEditor.cs
@@ -0,0 +1,36 @@
+﻿//@vadym udod
+
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditor.EventSystems;
+
+namespace hootybird.UI.Helpers
+{
+    [CustomEditor(typeof(SwipeHandler))]
+    public class SwipeHandlerEditor : EventTriggerEditor
+    {
+        private SerializedProperty methodProperty;
+        private SerializedProperty normalizeProperty;
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+
+            methodProperty = serializedObject.FindProperty("method");
+            normalizeProperty = serializedObject.FindProperty("normalizeSwipePoints");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+
+            EditorGUILayout.PropertyField(methodProperty);
+            EditorGUILayout.PropertyField(normalizeProperty);
+
+            serializedObject.ApplyModifiedProperties();
+
+            base.OnInspectorGUI();
+        }
+    }
+}
+#endif
