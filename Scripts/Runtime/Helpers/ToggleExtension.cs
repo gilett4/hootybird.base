459c5c1684d7586a9c4a569fd9bd0abc0e40d1a7
gilett4 <vadymudod@hotmail.com>
2021-01-17 15:56:12 +0200

    toggle extension added

---
+++ Scripts/Runtime/Helpers/ToggleExtension.cs
@@ -0,0 +1,37 @@
+﻿//@vadym udod
+
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+namespace hootybird.Helpers
+{
+    public class ToggleExtension : MonoBehaviour
+    {
+        [SerializeField]
+        private UnityEvent onToggleOn = default;
+        [SerializeField]
+        private UnityEvent onToggleOff = default;
+
+        private Toggle toggle;
+
+        protected void Awake()
+        {
+            toggle = GetComponent<Toggle>();
+
+            toggle.onValueChanged.AddListener(OnToggle);
+        }
+
+        private void OnToggle(bool value)
+        {
+            if (value)
+            {
+                onToggleOn?.Invoke();
+            }
+            else
+            {
+                onToggleOff?.Invoke();
+            }
+        }
+    }
+}
